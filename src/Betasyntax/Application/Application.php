ea5962c7a71323e01e996fc1c35f61827ccf89eb
Donovan Cameron <pynewbie@gmail.com>
2016-08-01 22:11:17 -0400

    adding application folder

---
+++ src/Betasyntax/Application/Application.php
@@ -0,0 +1,42 @@
+<?php namespace Betasyntax;
+
+use Closure;
+
+class Application 
+{
+  protected $version = '0.1';   
+
+  /**
+   * Create a new Illuminate application instance.
+   *
+   * @param  string|null  $basePath
+   * @return void
+   */
+  
+  public function __construct($basePath = null)
+  {
+    echo $this->version;
+    $this->registerBaseBindings();
+
+    $this->registerBaseServiceProviders();
+
+    $this->registerCoreContainerAliases();
+
+    if ($basePath) $this->setBasePath($basePath);
+  }
+
+  public function registerBaseBindings()
+  {
+
+  }
+
+  public function registerBaseServiceProviders()
+  {
+
+  }
+
+  public function registerCoreContainerAliases()
+  {
+
+  }
+}
