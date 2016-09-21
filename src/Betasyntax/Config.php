419e229235d48f5f9989d58cb8d66698a9c63f70
Donovan <pynewbie@gmail.com>
2016-09-21 00:50:08 -0400

    Update Config.php

--- src/Betasyntax/Config.php
+++ src/Betasyntax/Config.php
@@ -7,7 +7,11 @@ class Config {
   public $conf;
 
   public function __construct() {
-    $basepath = (string) app()->getBasePath().'/config/';
+    if (defined('PHPUNIT_BETASYNTAX_TESTSUITE') == true) {
+      $basepath = __dir__.'/config/';
+    } else {
+      $basepath = (string) app()->getBasePath().'/config/';
+    }
     $this->conf['app'] = Conf::load($basepath.'app.php');
     $this->conf['db'] = Conf::load($basepath.'db.php');
     $this->conf['email'] = Conf::load($basepath.'email.php');
