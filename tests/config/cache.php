418cd35b7dd96f4e83272953e22d759c9ea2fdab
Donovan Cameron <pynewbie@gmail.com>
2016-09-21 00:51:41 -0400

    setting up travis ci

---
+++ tests/config/cache.php
@@ -0,0 +1,14 @@
+<?php 
+return [
+    "default" => "",
+    "status"  => "",
+    "local"   => [
+        "mount" => "",
+        "weight" => '50'
+    ],
+    "memcache" => [
+        "host" => "127.0.0.1",
+        "port" => "11211",
+        "weight" => "50"
+    ]
+];
