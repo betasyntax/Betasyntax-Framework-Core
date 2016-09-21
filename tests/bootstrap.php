eb58efccb101588b527d1de60f38daa9d6c3f851
Donovan <pynewbie@gmail.com>
2016-09-20 20:37:46 -0400

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // require __DIR__.'/../../../vendor/autoload.php';
-require __DIR__.'/../../../../../vendor/autoload.php';
+require __DIR__.'/../../vendor/autoload.php';
 
 define("PHPUNIT_BETASYNTAX_TESTSUITE", true);
 
